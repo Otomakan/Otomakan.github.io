9e41357bdf351b6e6af884195bad338640b29e52
Jack Misteli <jgmisteli@gmail.com>
2020-01-14 22:27:22 +0900

    PWA resteye

--- snippets/resteye/sw.js
+++ snippets/resteye/sw.js
@@ -4,11 +4,11 @@ self.addEventListener('install', e => {
 	e.waitUntil(
 		caches.open(cacheName).then(cache => {
 			return cache.addAll([
-				'/',
-				'/index.html',
-				'/styles.css',
-				'/main.js',
-				'/manifest.json'
+				'./',
+				'./index.html',
+				'./styles.css',
+				'./main.js',
+				'./manifest.json'
 			]);
 		})
 	);
